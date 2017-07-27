3155ffe80061ca187c1d4734a64df5cd99013caa
thiagolocatellicdeoliveira <thiagolocatellicdeoliveira@gmail.com>
2017-07-27 20:30:02 -0300

    gitignore e settings

--- painelifc/painelifc/settings/base.py
+++ painelifc/painelifc/settings/base.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+MEDIA_PATH = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    STATIC_PATH,
+    MEDIA_PATH,
+)
